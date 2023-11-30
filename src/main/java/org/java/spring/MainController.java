17c0da0a526de16dfecbe87f01197e37d77ff3af
marcocareddu <saldumaccu@tuta.io>
2023-11-30 15:10:49 +0100

    Added if statements

--- src/main/java/org/java/spring/MainController.java
+++ src/main/java/org/java/spring/MainController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class MainController {
@@ -19,8 +20,17 @@ public class MainController {
 		List<Pizza> pizzas = pizzaService.finAll();
 		
 		model.addAttribute("list", pizzas);
-		model.addAttribute("object", "pizza");
+		
 		return "index";
+	}
+	
+	@GetMapping("/detail/{id}")
+	public String getPizza(Model model, @PathVariable int id) {
+
+		Pizza pizza = pizzaService.findById(id);
+		
+		model.addAttribute("id", pizza);
+		return "detail";
 	}
 	
 }
