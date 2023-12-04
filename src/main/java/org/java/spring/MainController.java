cef6942735053a3a8cbddfb8733fbe28c89163e0
marcocareddu <saldumaccu@tuta.io>
2023-12-04 11:47:45 +0100

    Added Create Crud to controller

--- src/main/java/org/java/spring/MainController.java
+++ src/main/java/org/java/spring/MainController.java
@@ -5,8 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -31,4 +34,23 @@ public class MainController {
 		model.addAttribute("list", pizza);
 		return "detail";
 	}
+	
+	@GetMapping("/create")
+	public String create(Model model) {
+		
+		model.addAttribute("pizza", new Pizza());
+		
+		return "create";
+	}
+	
+	@PostMapping("/create")
+	public String StorePizza(Model model,
+			@ModelAttribute Pizza pizza,
+			BindingResult bindingResult) {
+		
+		System.out.println("Pizza aggiunta");
+		
+		pizzaService.save(pizza);
+	return "redirect:/";
+}
 }
