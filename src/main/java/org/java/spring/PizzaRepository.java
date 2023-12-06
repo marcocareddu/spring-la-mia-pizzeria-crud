6fde9b81956a9ec202738dde824de443a313e9c0
marcocareddu <saldumaccu@tuta.io>
2023-12-06 12:25:37 +0100

    Added & implemented HEAD fragments

--- src/main/java/org/java/spring/PizzaRepository.java
+++ src/main/java/org/java/spring/PizzaRepository.java
@@ -5,6 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PizzaRepository extends JpaRepository<Pizza, Integer> {
 
-//	List<Pizza> findByNameContainingIgnoreCase(String searched);
 	List<Pizza> findByNameContainingOrDescriptionContainingIgnoreCase(String name, String description);
 	}
